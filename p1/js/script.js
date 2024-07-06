e5f747b60e37dea80e154d4636ef52c6ebbe007d
Angie20032022 <0951585769@aitec.edu.ec>
2024-07-06 15:59:46 -0500

    Add files via upload

---
+++ p1/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentloaded",funtion(){
+var boton
+document . getlementById('miBoton');
+boton. addeventlistener('click', cambiarcontenido);
+});
+function cambiarContenido(){
+ var parrafo-document . getelementbyrd('Parrafo");
+parrafo . innerHTML="E1 contenido ha sido cambiado"s
+parrafo.style.color-"red"
+}
